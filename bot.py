3faffc6940cdf5e70d60f1eaaade2c527e1ba3fd
Yuki <yotv2000tw@gmail.com>
2020-05-11 17:21:54 +0800

    天啊，好難寫= =

--- bot.py
+++ bot.py
@@ -21,9 +21,24 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(F"{member} leave")
 
+@bot.command()
+async def load(ctx,extension):
+    bot.load_extension(F'cmds.{extension}')
+    await ctx.send(F"Loaded {extension} done.")
+
+@bot.command()
+async def unload(ctx,extension):
+    bot.unload_extension(F'cmds.{extension}')
+    await ctx.send(F"Un-loaded {extension} done.")
+
+@bot.command()
+async def reload(ctx,extension):
+    bot.reload_extension(F'cmds.{extension}')
+    await ctx.send(F"Re-loaded {extension} done.")
+
 for Filename in os.listdir('./cmds'):
     if Filename.endswith('.py'):
         bot.load_extension(f"cmds.{Filename[:-3]}")
-        
+
 if __name__ == "__main__":
     bot.run(jdata['TOKEN'])
--- cmds/react.py
+++ cmds/react.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 from core.classes import Cog_extension
 import json
@@ -8,7 +9,14 @@ with open('setting.json',mode='r',encoding='utf8') as jFile:
 class react(Cog_extension):
     @commands.command()
     async def 圖片(self,ctx):
-        pic = discord.File(jdata['Pic'])
+        random_pic = random.choice(jdata['Pic'])
+        pic = discord.File(random_pic)
         await ctx.send(file= pic)
+
+    @commands.command()
+    async def web(self,ctx):
+        random_pic = random.choice(jdata['url_pic'])
+        await ctx.send(random_pic)
+
 def setup(bot):
     bot.add_cog(react(bot))
